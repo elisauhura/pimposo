30d764efa8c3c4d5cbbb09566878474eb3f4168a
Vitor Silva <sirmochi45@gmail.com>
2018-09-25 21:57:59 -0300

    Add initial data structures

---
+++ element.h
@@ -0,0 +1,60 @@
+/* Element.h - By Vitor Silva */
+
+typedef char * str;
+typedef short int Bool;
+
+typedef struct {
+	str Name;
+	str Articles[4];
+	str LongDesc;
+	str ShortDesc;
+	Element ObjList[];
+	void * Attributes;
+	str Action[];
+	str Animation;
+
+	/*OOP*/
+	void * __Super;
+	void * __Child;
+	void * __Self;
+	str __Class;
+} Element;
+
+typedef struct {
+	void * To; /*Place*/
+	Bool Closed;
+	
+	/*OOP*/
+	void * __Super; /*Element*/
+	void * __Child;
+	void * __Self;
+	str __Class;
+} Path;
+
+typedef struct {
+	void Exit[];
+	
+	/*OOP*/
+	void * __Super; /*Element*/
+	void * __Child;
+	void * __Self;
+	str __Class;
+} Place;
+
+typedef struct {
+	str Adjectives[];
+	Bool Hidden;
+
+	/*OOP*/
+	void * __Super; /*Element*/
+	void * __Child;
+	void * __Self;
+	str __Class;
+} Object;
+
+/* @Actions
+ - lookup -> return object description
+ - catch -> send object from the place to the player
+ - throw -> send object from the player to the place
+ ...
+*/
