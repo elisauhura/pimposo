fda9ab01a7098639669a0c7d790bb9af962de8a2
Vitor Silva <sirmochi45@gmail.com>
2018-09-30 23:08:44 -0300

    Add verbosity

--- element.test.c
+++ element.test.c
@@ -3,8 +3,15 @@
 #include "stdio.h"
 
 int main(void) {
+	puts("Testing Element");
+
+	puts("Create Element");
 	Element * e = newElement("Pimp");
+
+	puts("Getting its name (Pimp)");
 	puts(e->Name);
+
+	puts("Freeing it");
 	freeElement(e);
 
 	puts("Test Complete");
