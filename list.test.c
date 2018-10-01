c6c09656133153845a40c6afa644036131bf257c
Vitor Silva <sirmochi45@gmail.com>
2018-09-30 23:12:24 -0300

    Add verbosity

--- list.test.c
+++ list.test.c
@@ -6,20 +6,29 @@
 #include "list.h"
 
 int main() {
+    puts("Testing List");
     int i[4] = {1,2,3,4};
+    puts("Creating List");
     List l = List_create();
+    puts("Adding elements");
     l = List_insert(l, i + 0, "um");
     l = List_insert(l, i + 1, "dois");
     l = List_insert(l, i + 2, "tres");
     l = List_insert(l, i + 3, "quatro");
+    puts("Quering some elements (3 and 1)");
     int * q = List_query(l, "tres");
     int * p = List_query(l, "um");
     printf("%d\n", * q);
     printf("%d\n", * p);
+    puts("Removing 1");
     List_Pack pack = List_remove(l, "um");
     l = pack.l;
+    puts("Checking if 1 was removed");
     if(List_query(l, "um") == NULL)
         puts("Success");
+    puts("Freeing it");
     List_destroy(l);
+
+    puts("Test Complete");
     return 0;
 }
